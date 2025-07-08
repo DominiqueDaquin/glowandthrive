90a0cff45f95ffdbb0f2fcf6c6aadeb7c53ea343
DominiqueDaquin <kouakepdominique@gmail.com>
2025-07-08 15:22:52 +0100

    tagmanager

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next"
 import { Poppins } from "next/font/google"
 import Link from "next/link"
 
-const poppins = Poppins({ 
+const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
   display: "swap"
@@ -34,7 +34,35 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
+
+      <head>
+        {/* <!-- Google Tag Manager --> */}
+                <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MCM83MXN');
+            `,
+          }}
+        />
+        {/* <!-- End Google Tag Manager --> */}
+      </head>
+
       <body className={poppins.className}>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+           <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MCM83MXN"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
+
         <nav className="bg-white shadow-sm border-b">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between h-16">
